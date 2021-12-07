c06d4ff5fd12eb4da9b62baac4dd14695650db0a
MouchenHung <Mouchen.Hung@quantatw.com>
2021-12-07 19:18:59 +0800

    v1.4.5 - Modify platform code

--- src/platform/plat_i2c_slave.c
+++ src/platform/plat_i2c_slave.c
@@ -3,7 +3,7 @@
   FILE: plat_i2c_slave.c
   DESCRIPTION: Provide "util_init_I2C_slave()" for init slave config.
   AUTHOR: MouchenHung
-  DATE/VERSION: 2021.12.06 - v1.3
+  DATE/VERSION: 2021.12.06 - v1.2
   Note: 
     (1) "plat_i2c_slave.h" is included by "hal_i2c_slave.h"
 */
@@ -19,8 +19,8 @@ LOG_MODULE_DECLARE(mc_i2c_slave);
 
 /* I2C slave init-config table, disable if slave address equal 0xFF */
 static const struct _i2c_slave_config I2C_SLAVE_CFG_TABLE[MAX_SLAVE_NUM] = {
-  { 0x60,       0x5 },
-  { 0x40,       0x5 },
+  { 0x60,       0x05 },
+  { 0x40,       0x05 },
   { MAX_ADDR,   0x0A },
   { MAX_ADDR,   0x0A },
   { MAX_ADDR,   0x0A },
@@ -43,11 +43,7 @@ void util_init_I2C_slave(void) {
 
   for (int i=0; i<MAX_SLAVE_NUM; i++){
     if (I2C_SLAVE_CFG_TABLE[i].address != MAX_ADDR){
-      struct _i2c_slave_config cur_cfg;
-      cur_cfg.address = I2C_SLAVE_CFG_TABLE[i].address;
-      cur_cfg.i2c_msg_count = I2C_SLAVE_CFG_TABLE[i].i2c_msg_count;
-
-      ret = i2c_slave_control(i, &cur_cfg, I2C_CONTROL_REGISTER);
+      ret = i2c_slave_control(i, &I2C_SLAVE_CFG_TABLE[i], I2C_CONTROL_REGISTER);
       if (ret)
         LOG_ERR("Init bus[%d] slave - failed, cause of errorcode[%d]\n", i, ret);
       else
